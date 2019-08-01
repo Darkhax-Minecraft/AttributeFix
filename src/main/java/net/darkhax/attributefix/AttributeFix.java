4d2660f9aa205ba9032567ea69f30abcd9c0bac3
Tyler Hancock <darklime@live.ca>
2019-08-01 16:31:53 -0600

    Ported to 1.13.2

--- src/main/java/net/darkhax/attributefix/AttributeFix.java
+++ src/main/java/net/darkhax/attributefix/AttributeFix.java
@@ -1,40 +1,25 @@
 package net.darkhax.attributefix;
 
-import static net.minecraft.entity.SharedMonsterAttributes.ARMOR;
-import static net.minecraft.entity.SharedMonsterAttributes.ARMOR_TOUGHNESS;
-import static net.minecraft.entity.SharedMonsterAttributes.ATTACK_DAMAGE;
-import static net.minecraft.entity.SharedMonsterAttributes.ATTACK_SPEED;
-import static net.minecraft.entity.SharedMonsterAttributes.FLYING_SPEED;
-import static net.minecraft.entity.SharedMonsterAttributes.FOLLOW_RANGE;
-import static net.minecraft.entity.SharedMonsterAttributes.KNOCKBACK_RESISTANCE;
-import static net.minecraft.entity.SharedMonsterAttributes.LUCK;
-import static net.minecraft.entity.SharedMonsterAttributes.MAX_HEALTH;
-import static net.minecraft.entity.SharedMonsterAttributes.MOVEMENT_SPEED;
-
-import net.minecraft.entity.ai.attributes.IAttribute;
-import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = "attributefix", name = "Attribute Fix", version = "@VERSION@", certificateFingerprint = "@FINGERPRINT@")
+@Mod("attributefix")
 public class AttributeFix {
+	
+    private final ConfigurationHandler config;
+    
+    public AttributeFix () {
+    	
+    	this.config = new ConfigurationHandler();
+    	ModLoadingContext.get().registerConfig(Type.COMMON, config.getSpec());
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+    
+    private void setup (FMLCommonSetupEvent event) {
 
-    public static final IAttribute[] ATTRIBUTES = new IAttribute[] { MAX_HEALTH, FOLLOW_RANGE, KNOCKBACK_RESISTANCE, MOVEMENT_SPEED, FLYING_SPEED, ATTACK_DAMAGE, ATTACK_SPEED, ARMOR, ARMOR_TOUGHNESS, LUCK };
-
-    @EventHandler
-    public void preInit (FMLPreInitializationEvent event) {
-
-        final ConfigurationHandler config = new ConfigurationHandler(event.getSuggestedConfigurationFile());
-
-        for (final IAttribute attribute : ATTRIBUTES) {
-
-            if (attribute instanceof RangedAttribute) {
-
-                config.configureRangedAttribute((RangedAttribute) attribute);
-            }
-        }
-
-        config.syncConfigData();
+    	this.config.applyChanges();
     }
 }
--- src/main/java/net/darkhax/attributefix/ConfigurationHandler.java
+++ src/main/java/net/darkhax/attributefix/ConfigurationHandler.java
@@ -1,44 +1,81 @@
 package net.darkhax.attributefix;
 
-import java.io.File;
+import static net.minecraft.entity.SharedMonsterAttributes.ARMOR;
+import static net.minecraft.entity.SharedMonsterAttributes.ARMOR_TOUGHNESS;
+import static net.minecraft.entity.SharedMonsterAttributes.ATTACK_DAMAGE;
+import static net.minecraft.entity.SharedMonsterAttributes.ATTACK_SPEED;
+import static net.minecraft.entity.SharedMonsterAttributes.FLYING_SPEED;
+import static net.minecraft.entity.SharedMonsterAttributes.FOLLOW_RANGE;
+import static net.minecraft.entity.SharedMonsterAttributes.KNOCKBACK_RESISTANCE;
+import static net.minecraft.entity.SharedMonsterAttributes.LUCK;
+import static net.minecraft.entity.SharedMonsterAttributes.MAX_HEALTH;
+import static net.minecraft.entity.SharedMonsterAttributes.MOVEMENT_SPEED;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
 
+import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
-import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.ForgeConfigSpec;
 
 public class ConfigurationHandler {
 
-    public static Configuration config;
+    private static final IAttribute[] ATTRIBUTES = new IAttribute[] { MAX_HEALTH, FOLLOW_RANGE, KNOCKBACK_RESISTANCE, MOVEMENT_SPEED, FLYING_SPEED, ATTACK_DAMAGE, ATTACK_SPEED, ARMOR, ARMOR_TOUGHNESS, LUCK };
 
-    public ConfigurationHandler (File file) {
-
-        config = new Configuration(file);
-    }
-
-    public void configureRangedAttribute (RangedAttribute attribute) {
-
-        final String name = StringUtils.capitalize(attribute.getName().replace("generic.", ""));
-
-        attribute.maximumValue = config.getFloat("max" + name, "attributes", this.getAdjustedAmount((float) attribute.maximumValue), Float.MIN_VALUE, Float.MAX_VALUE, "The maximum amount for the " + name + " attribute.");
-        attribute.minimumValue = config.getFloat("min" + name, "attributes", (float) attribute.minimumValue, Float.MIN_VALUE, Float.MAX_VALUE, "The minimum amount for the " + name + " attribute.");
-
-        if (attribute.minimumValue > attribute.maximumValue) {
-
-            throw new IllegalArgumentException("The " + name + " attribute was configured incorrectly! Minimum must not be greater than maximum!");
-        }
-    }
-
-    private float getAdjustedAmount (float amount) {
-
-        return Math.max(amount, 65536f);
-    }
-
-    public void syncConfigData () {
-
-        if (config.hasChanged()) {
-
-            config.save();
-        }
+	private final Map<RangedAttribute, AttributeData> attributes = new HashMap<>();
+	private final ForgeConfigSpec spec;
+	
+	public ConfigurationHandler () {
+		
+		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+		
+		for (IAttribute attribute : ATTRIBUTES) {
+			
+			if (attribute instanceof RangedAttribute) {
+				
+				RangedAttribute rangedAttribute = (RangedAttribute) attribute;
+				attributes.put(rangedAttribute, new AttributeData(rangedAttribute, builder));
+			}
+		}
+		
+		this.spec = builder.build();
+	}
+	
+	public ForgeConfigSpec getSpec() {
+		
+		return spec;
+	}
+	
+	public void applyChanges() {
+		
+		for (IAttribute attribute : ATTRIBUTES) {
+			
+			if (attribute instanceof RangedAttribute) {
+				
+				RangedAttribute rangedAttribute = (RangedAttribute) attribute;
+				AttributeData data = attributes.get(rangedAttribute);
+				
+				rangedAttribute.minimumValue = data.min.get();
+				rangedAttribute.maximumValue = data.max.get();
+			}
+		}
+	}
+	
+    private class AttributeData {
+    	
+    	private final ForgeConfigSpec.DoubleValue min;
+    	private final ForgeConfigSpec.DoubleValue max;
+    	
+    	public AttributeData(RangedAttribute attribute, ForgeConfigSpec.Builder builder) {
+    		
+    		String group = attribute.getName().replace("generic.", "").replace(".", "").toLowerCase();
+    		
+    		builder.comment("Values for the " + group + " attribute.");
+    		builder.push(group);
+    		min = builder.defineInRange("min", attribute.minimumValue, 0d, Double.MAX_VALUE);
+    		max = builder.defineInRange("max", Math.max(attribute.minimumValue, 65536d), 0d, Double.MAX_VALUE);
+    		
+    		builder.pop();
+    	}
     }
 }
