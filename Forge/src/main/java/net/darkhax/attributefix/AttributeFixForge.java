b71128ac34f65c9575154d974d5b220866e05652
Tyler Hancock <darklime@live.ca>
2021-12-18 06:23:16 -0700

    Update for Minecraft 1.18

--- Forge/src/main/java/net/darkhax/attributefix/AttributeFixForge.java
+++ Forge/src/main/java/net/darkhax/attributefix/AttributeFixForge.java
@@ -3,7 +3,6 @@ package net.darkhax.attributefix;
 import net.darkhax.attributefix.config.AttributeConfig;
 import net.darkhax.attributefix.temp.RegistryHelper;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
@@ -18,13 +17,5 @@ public class AttributeFixForge {
 
         RegistryHelper<Attribute> registry = new AttributeRegistryHelper();
         AttributeConfig.load(FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID + ".json").toFile(), registry).applyChanges(registry);
-
-        for (Attribute attribute : registry.getValues()) {
-
-            if (attribute instanceof RangedAttribute ranged) {
-
-                Constants.LOG.info("{}    {}-{}    {}", attribute.getRegistryName(), ranged.getMinValue(), ranged.getMaxValue(), ranged.sanitizeValue(15_000d));
-            }
-        }
     }
 }
